5dc66432e5adb609c21add99c35088602e59a25a
Julian Rivera <julianarevircoins@gmail.com>
2025-10-07 19:31:39 -0400

    create new entry, entrycard component

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -2,6 +2,7 @@ import NewEntryCard from "@/components/NewEntryCard"
 import EntryCard from "@/components/EntryCard"
 import { getUserByClerkID } from "@/utils/auth"
 import { prisma } from "@/utils/db"
+import Link from "next/link"
 
 
 
@@ -29,7 +30,12 @@ return(
         <h2 className="text-3xl mb-8">Journal</h2>
     <div className="grid grid-cols-3 gap-4 p-10">
         <NewEntryCard/>
-        {entries.map(entry=> (<EntryCard key={entry.id} entry={entry}/>))}
+        {entries.map(entry=> 
+            <Link href={`/journal/${entry.id}`} key={entry.id}>
+            <EntryCard  entry={entry}/>
+            </Link>
+            )}
+            
     </div>
     </div>
 )
